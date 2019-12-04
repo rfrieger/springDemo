a146827a630777643d0743fb35f092568b3b55d1
rfrieger <rfrieger@gmail.com>
2019-12-03 19:13:52 -0500

    added stubs

---
+++ src/main/java/com/example/demo/models/account.java
@@ -0,0 +1,55 @@
+package com.example.demo.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class account {
+
+    public account() {
+    }
+
+    @Id
+    private Integer accountNumber;
+
+    private String name;
+
+    private String address;
+
+    private Double balance;
+
+
+    public Integer getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(Integer accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+
+}
